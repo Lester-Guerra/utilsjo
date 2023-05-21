9582a0990cd771c4580c6f9fdc92fe8aa7c0d6be
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-05-20 21:47:37 -0600

    v 0.1

--- funcionesjo/funcionesjo.py
+++ funcionesjo/funcionesjo.py
@@ -1,110 +1,190 @@
+import calendar
+from typing import List, Tuple
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 def hoy(formato="%Y-%m-%d", inicio_de_mes=False) -> str:
+    """
+    Esta función retorna la fecha de hoy en el formato proporcionado.
+
+    Parámetros:
+    formato: str - Formato en el que se desea obtener la fecha. (Por defecto es "%Y-%m-%d").
+    inicio_de_mes: bool - Si es True, el día será el primer día del mes (Por defecto es False).
+
+    Retorna:
+    str - La fecha de hoy en el formato proporcionado.
+
+    Excepciones:
+    ValueError - Si el formato proporcionado no es válido.
+    """
+    if not isinstance(formato, str):
+        raise ValueError("El formato debe ser una cadena de texto.")
+
     if inicio_de_mes and "%d" in formato:
         formato = formato.replace("%d", "01")
     return datetime.today().strftime(formato)
 
 def day_after(fecha: str, formato='%Y-%m-%d') -> str:
-    fecha_datetime = datetime.strptime(fecha, formato)
-    fecha_mas_un_dia = fecha_datetime + timedelta(days=1)
-    fecha_texto = fecha_mas_un_dia.strftime(formato)
-    return fecha_texto
+    """
+    Esta función retorna la fecha que se obtiene al agregar un día a la fecha proporcionada.
+
+    Parámetros:
+    fecha: str - Fecha a la que se desea agregar un día.
+    formato: str - Formato en el que se proporciona la fecha. (Por defecto es '%Y-%m-%d')
+
+    Retorna:
+    str - La fecha que se obtiene al agregar un día a la fecha proporcionada en el formato original.
+
+    Excepciones:
+    ValueError - Si la fecha o el formato proporcionado no es válido.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        fecha_mas_un_dia = fecha_datetime + timedelta(days=1)
+        fecha_texto = fecha_mas_un_dia.strftime(formato)
+        return fecha_texto
+    except ValueError:
+        raise ValueError("La fecha o el formato proporcionado no es válido.")
+
+from dateutil.relativedelta import relativedelta
 
 def year_ago(fecha: str, formato='%Y-%m-%d', inicio_de_mes=False, inicio_de_anio=False) -> str:
-    if "-" in fecha:
-        separador = "-"
-    elif "/" in fecha:
-        separador = "/"
-    formato = formato.replace('%', '').split(separador)
-    fecha = fecha.split(separador)
-    pos_Y = formato.index('Y')
-    fecha[pos_Y] = str(int(fecha[pos_Y]) - 1)
-    if inicio_de_mes:
-        pos_d = formato.index('d')
-        fecha[pos_d] = "01"
-    elif inicio_de_anio:
-        pos_d = formato.index('d')
-        pos_m = formato.index('m')
-        fecha[pos_d] = "01"
-        fecha[pos_m] = "01"
-    return f"{separador}".join(fecha)
+    """
+    Esta función retorna la fecha que se obtiene al restar un año a la fecha proporcionada.
+
+    Parámetros:
+    fecha: str - Fecha a la que se desea restar un año.
+    formato: str - Formato en el que se proporciona la fecha. (Por defecto es '%Y-%m-%d')
+    inicio_de_mes: bool - Si es True, el día será el primer día del mes. (Por defecto es False)
+    inicio_de_anio: bool - Si es True, el mes y el día serán el primer mes y día del año. (Por defecto es False)
+
+    Retorna:
+    str - La fecha que se obtiene al restar un año a la fecha proporcionada en el formato original.
+
+    Excepciones:
+    ValueError - Si la fecha o el formato proporcionado no es válido.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        fecha_un_ano_antes = fecha_datetime - relativedelta(years=1)
+        if inicio_de_mes:
+            fecha_un_ano_antes = fecha_un_ano_antes.replace(day=1)
+        if inicio_de_anio:
+            fecha_un_ano_antes = fecha_un_ano_antes.replace(month=1, day=1)
+        fecha_texto = fecha_un_ano_antes.strftime(formato)
+        return fecha_texto
+    except ValueError:
+        raise ValueError("La fecha o el formato proporcionado no es válido.")
 
 def month_after(fecha: str, formato='%Y-%m-%d') -> str:
-    if "-" in fecha:
-        separador = "-"
-    elif "/" in fecha:
-        separador = "/"
-    formato = formato.replace('%', '').split(separador)
-    fecha = fecha.split(separador)
-    pos_m = formato.index('m')
-    pos_Y = formato.index('Y')
-    if fecha[pos_m] == "12":
-        fecha[pos_m] = "01"
-        fecha[pos_Y] = str(int(fecha[pos_Y]) + 1)
-    else:
-        fecha[pos_m] = str(int(fecha[pos_m]) + 1).rjust(2, "0")
-    return f"{separador}".join(fecha)
+    """
+    Esta función retorna la fecha que se obtiene al agregar un mes a la fecha proporcionada.
+
+    Parámetros:
+    fecha: str - Fecha a la que se desea agregar un mes.
+    formato: str - Formato en el que se proporciona la fecha. (Por defecto es '%Y-%m-%d')
+
+    Retorna:
+    str - La fecha que se obtiene al agregar un mes a la fecha proporcionada en el formato original.
+
+    Excepciones:
+    ValueError - Si la fecha o el formato proporcionado no es válido.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        fecha_un_mes_despues = fecha_datetime + relativedelta(months=1)
+        fecha_texto = fecha_un_mes_despues.strftime(formato)
+        return fecha_texto
+    except ValueError:
+        raise ValueError("La fecha o el formato proporcionado no es válido.")
 
 def month_before(fecha: str, formato='%Y-%m-%d') -> str:
-    if "-" in fecha:
-        separador = "-"
-    elif "/" in fecha:
-        separador = "/"
-    formato = formato.replace('%', '').split(separador)
-    fecha = fecha.split(separador)
-    pos_m = formato.index('m')
-    pos_Y = formato.index('Y')
-    if fecha[pos_m] == "01":
-        fecha[pos_m] = "12"
-        fecha[pos_Y] = str(int(fecha[pos_Y]) - 1)
-    else:
-        fecha[pos_m] = str(int(fecha[pos_m]) - 1).rjust(2, "0")
-    return f"{separador}".join(fecha)
+    """
+    Esta función retorna la fecha que se obtiene al restar un mes a la fecha proporcionada.
 
-def date_mini(fecha: str, formato='%Y-%m-%d') -> str: # formato %Y-%m
-    if "-" in fecha:
-        separador = "-"
-    elif "/" in fecha:
-        separador = "/"
-    formato = formato.replace('%', '').split(separador)
-    fecha = fecha.split(separador)
-    pos_m = formato.index('m')
-    pos_Y = formato.index('Y')
-    return f"{separador}".join((fecha[pos_Y], fecha[pos_m]))
+    Parámetros:
+    fecha: str - Fecha a la que se desea restar un mes.
+    formato: str - Formato en el que se proporciona la fecha. (Por defecto es '%Y-%m-%d')
+
+    Retorna:
+    str - La fecha que se obtiene al restar un mes a la fecha proporcionada en el formato original.
+
+    Excepciones:
+    ValueError - Si la fecha o el formato proporcionado no es válido.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        fecha_un_mes_antes = fecha_datetime - relativedelta(months=1)
+        fecha_texto = fecha_un_mes_antes.strftime(formato)
+        return fecha_texto
+    except ValueError:
+        raise ValueError("La fecha o el formato proporcionado no es válido.")
+
+def date_mini(fecha: str, formato='%Y-%m-%d') -> str:
+    """
+    Esta función retorna la fecha proporcionada en formato "Año-Mes".
+
+    Parámetros:
+    fecha: str - Fecha que se desea convertir.
+    formato: str - Formato en el que se proporciona la fecha. (Por defecto es '%Y-%m-%d')
+
+    Retorna:
+    str - La fecha proporcionada en formato "Año-Mes".
+
+    Excepciones:
+    ValueError - Si la fecha o el formato proporcionado no es válido.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        fecha_mini = fecha_datetime.strftime('%Y-%m')
+        return fecha_mini
+    except ValueError:
+        raise ValueError("La fecha o el formato proporcionado no es válido.")
 
 def mes_by_ordinal(ordinal: str, abreviado=True, mes_anterior=False) -> str:
-    if type(ordinal) is not str:
-        ordinal = str(ordinal).rjust(2, '0')
-    ORDINALES_MES = {
-        "01":"Enero",
-        "02":"Febrero",
-        "03":"Marzo",
-        "04":"Abril",
-        "05":"Mayo",
-        "06":"Junio",
-        "07":"Julio",
-        "08":"Agosto",
-        "09":"Septiembre",
-        "10":"Octubre",
-        "11":"Noviembre",
-        "12":"Diciembre"}
-    try:
-        if not mes_anterior:
-            if abreviado:
-                return ORDINALES_MES[ordinal][0:3]
-            else:
-                return ORDINALES_MES[ordinal]
-        else:
-            if abreviado:
-                ordinal = str(int(ordinal) - 1).rjust(2, "0")
-                return ORDINALES_MES[ordinal][0:3]
-            else:
-                return ORDINALES_MES[ordinal]
-    except:
-        return "NaN"
+    """
+    Esta función retorna el nombre del mes correspondiente al número ordinal proporcionado.
 
-def mes_anio_by_abreviacion(abreviacion: str, capON: bool=False, MMAA: bool=False) -> str:
+    Parámetros:
+    ordinal: str - Número ordinal del mes.
+    abreviado: bool - Si se debe retornar el nombre del mes abreviado. (Por defecto es True)
+    mes_anterior: bool - Si se debe retornar el nombre del mes anterior al proporcionado. (Por defecto es False)
+
+    Retorna:
+    str - El nombre del mes correspondiente al número ordinal proporcionado.
+
+    Excepciones:
+    ValueError - Si el número ordinal está fuera de rango.
+    """
+    try:
+        ordinal = int(ordinal)
+        if mes_anterior:
+            ordinal -= 1
+        if not 1 <= ordinal <= 12:
+            raise ValueError("El número ordinal está fuera de rango.")
+        nombres_meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+        nombre_mes = nombres_meses[ordinal - 1]
+        if abreviado:
+            nombre_mes = nombre_mes[0:3]
+        return nombre_mes
+    except:
+        raise ValueError("La entrada no es válida.")
+
+def mes_anio_by_abreviacion(abreviacion: str, mayuscula: bool=False, mmaa: bool=False) -> str:
+    """
+    Esta función convierte una abreviatura de mes-año a una forma más legible.
+
+    Parámetros:
+    abreviacion: str - Abreviatura de mes-año.
+    capON: bool - Si la salida debe tener la primera letra en mayúsculas. (Por defecto es False)
+    mmaa: bool - Si la abreviatura es en formato mes-año. (Por defecto es False)
+
+    Retorna:
+    str - La abreviatura de mes-año en una forma más legible.
+
+    Excepciones:
+    ValueError - Si la abreviatura no es válida.
+    """
     ABREVIATURAS = {
         "Ene":"enero",
         "Feb":"febrero",
@@ -119,70 +199,116 @@ def mes_anio_by_abreviacion(abreviacion: str, capON: bool=False, MMAA: bool=Fals
         "Nov":"noviembre",
         "Dic":"diciembre"}
     try:
-        if MMAA:
-            mes = ABREVIATURAS[abreviacion.split("-")[0]]
-            anio = abreviacion.split("-")[1]
-        else:
-            mes = ABREVIATURAS[abreviacion.split("-")[1]]
-            anio = abreviacion.split("-")[0]
-        if capON:
-            return f"{mes} {anio}".capitalize()
-        else:
-            return f"{mes} {anio}"
-    except:
-        return "NaN"
+        mes, anio = abreviacion.split("-")
+        if mmaa:
+            mes, anio = anio, mes
+        mes = ABREVIATURAS[mes]
+        if mayuscula:
+            mes = mes.capitalize()
+        return f"{mes} {anio}"
+    except KeyError:
+        raise ValueError("La abreviatura no es válida.")
 
 def anio_mes(fecha: str, formato='%Y-%m-%d') -> str:
-    if "-" in fecha:
-        separador = "-"
-    elif "/" in fecha:
-        separador = "/"
-    formato = formato.replace('%', '').split(separador)
-    fecha = fecha.split(separador)
-    pos_m = formato.index('m')
-    pos_Y = formato.index('Y')
-    return "-".join((fecha[pos_Y], mes_by_ordinal(fecha[pos_m])))
+    """
+    Esta función toma una fecha y devuelve el año seguido del nombre del mes.
+
+    Parámetros:
+    fecha: str - La fecha en formato de cadena de texto.
+    formato: str - El formato de la fecha en formato de cadena de texto. (Por defecto es '%Y-%m-%d')
+
+    Retorna:
+    str - El año seguido del nombre del mes.
+
+    Excepciones:
+    ValueError - Si la fecha no cumple con el formato.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        return fecha_datetime.strftime('%Y-%b')
+    except ValueError:
+        raise ValueError("La fecha no cumple con el formato.")
 
 def ultimo_dia_del_mes(fecha:str, formato='%Y-%m-%d') -> str:
-    if "-" in fecha:
-        separador = "-"
-    elif "/" in fecha:
-        separador = "/"
-    formato = formato.replace('%', '').split(separador)
-    fecha = fecha.split(separador)
-    pos_m = formato.index('m')
-    pos_d = formato.index('d')
-    pos_Y = formato.index('Y')
-    if fecha[pos_m] == 2:
-        if es_bisiesto(int(fecha[pos_Y])):
-           fecha[pos_d] = "29"
-        else:
-           fecha[pos_d] = "28"
-    elif int(fecha[pos_m]) in (4, 6, 9, 11):
-        fecha[pos_d] = "30"
-    else:
-        fecha[pos_d] = "31"
+    """
+    Esta función toma una fecha y devuelve la fecha correspondiente al último día del mes de la fecha proporcionada.
 
-    return f"{separador}".join(fecha)
+    Parámetros:
+    fecha: str - La fecha en formato de cadena de texto.
+    formato: str - El formato de la fecha en formato de cadena de texto. (Por defecto es '%Y-%m-%d')
 
-def es_bisiesto(anio):
-	return not anio % 4 and (anio % 100 or not anio % 400)
+    Retorna:
+    str - La fecha correspondiente al último día del mes de la fecha proporcionada.
 
-def invertir_orden(data, Qfecha: bool = True):
+    Excepciones:
+    ValueError - Si la fecha no cumple con el formato.
+    """
+    try:
+        fecha_datetime = datetime.strptime(fecha, formato)
+        ultimo_dia = calendar.monthrange(fecha_datetime.year, fecha_datetime.month)[1]
+        fecha_datetime = fecha_datetime.replace(day=ultimo_dia)
+        return fecha_datetime.strftime(formato)
+    except ValueError:
+        raise ValueError("La fecha no cumple con el formato.")
+
+def es_bisiesto(anio: int) -> bool:
+    """
+    Esta función verifica si un año es bisiesto.
+
+    Parámetros:
+    anio: int - El año a verificar.
+
+    Retorna:
+    bool - Verdadero si el año es bisiesto, falso de lo contrario.
+    """
+    return anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0)
+
+from typing import List, Tuple
+
+def invertir_orden(data: List[Tuple], Qfecha: bool = True) -> List[Tuple]:
+    """
+    Esta función toma una lista de tuplas y devuelve una lista de tuplas con los elementos en orden invertido.
+    Si el primer elemento de la tupla es una fecha, también se invierte el orden de la fecha.
+
+    Parámetros:
+    data: List[Tuple] - La lista de tuplas a invertir.
+    Qfecha: bool - Indicador si el primer elemento de la tupla es una fecha.
+
+    Retorna:
+    List[Tuple] - La lista de tuplas con el orden invertido.
+
+    Excepciones:
+    ValueError - Si el primer elemento de la tupla no cumple con el formato de fecha.
+    """
     data_salida = []
     if Qfecha:
         for dato in data:
-            fecha = "-".join(dato[0].split("-")[::-1])
-            data_salida.append((fecha, dato[1]))
+            try:
+                fecha = "-".join(dato[0].split("-")[::-1])
+                data_salida.append((fecha, dato[1]))
+            except:
+                raise ValueError("El primer elemento de la tupla no cumple con el formato de fecha.")
     else:
         for dato in data:
             data_salida.append(dato[::-1])
-    return tuple(data_salida)
+    return data_salida
 
-def r0und(numero: float, precicion: int=2) -> float:
+def r0und(numero: float, precicion: int = 2) -> float:
+    """
+    Esta función redondea un número con una precisión dada.
+
+    Parámetros:
+    numero: float - El número a redondear.
+    precicion: int - La cantidad de dígitos decimales a redondear. (Por defecto es 2)
+
+    Retorna:
+    float - El número redondeado.
+
+    Excepciones:
+    ValueError - Si el número no es un float o la precisión no es un int.
+    """
+    if not isinstance(numero, float) or not isinstance(precicion, int):
+        raise ValueError("El número debe ser un float y la precisión debe ser un int.")
+    
     numero = round(numero, precicion)
-    if numero == -0.0:
-        numero = 0.0
-    else:
-        numero = numero
-    return numero
+    return 0.0 if numero == -0.0 else numero
--- tests/test_funcionesjo.py
+++ tests/test_funcionesjo.py
@@ -1,1 +1,59 @@
-from .. import funcionesjo
+import unittest
+from funcionesjo import (
+    hoy,
+    day_after,
+    year_ago,
+    month_after,
+    month_before,
+    date_mini,
+    mes_by_ordinal,
+    mes_anio_by_abreviacion,
+    anio_mes,
+    ultimo_dia_del_mes,
+    es_bisiesto,
+    invertir_orden,
+    r0und
+)
+
+class TestFuncionesjo(unittest.TestCase):
+    def test_hoy(self):
+        self.assertIsInstance(hoy(), str)
+
+    def test_day_after(self):
+        self.assertEqual(day_after('2023-01-31'), '2023-02-01')
+
+    def test_year_ago(self):
+        self.assertEqual(year_ago('2024-02-29'), '2023-02-28')
+
+    def test_month_after(self):
+        self.assertEqual(month_after('2023-01-31'), '2023-02-28')
+
+    def test_month_before(self):
+        self.assertEqual(month_before('2023-03-31'), '2023-02-28')
+
+    def test_date_mini(self):
+        self.assertEqual(date_mini('2023-03-31'), '2023-03')
+
+    def test_mes_by_ordinal(self):
+        self.assertEqual(mes_by_ordinal('3'), 'Mar')
+
+    def test_mes_anio_by_abreviacion(self):
+        self.assertEqual(mes_anio_by_abreviacion('Mar-2023'), 'marzo 2023')
+
+    def test_anio_mes(self):
+        self.assertEqual(anio_mes('2023-03-31'), '2023-Mar')
+
+    def test_ultimo_dia_del_mes(self):
+        self.assertEqual(ultimo_dia_del_mes('2023-02-01'), '2023-02-28')
+
+    def test_es_bisiesto(self):
+        self.assertTrue(es_bisiesto(2024))
+
+    def test_invertir_orden(self):
+        self.assertEqual(invertir_orden([('2023-01-31', 'data')]), [('31-01-2023', 'data')])
+
+    def test_r0und(self):
+        self.assertEqual(r0und(1.11111, 3), 1.111)
+
+if __name__ == '__main__':
+    unittest.main()
