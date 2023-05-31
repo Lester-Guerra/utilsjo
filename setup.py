a57d242c1c24c0bb153bb2c98c5669340c007823
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-05-31 10:46:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='funcionesjo',
-    version='0.2.5',
+    version='0.2.6',
     author='Luis Alfredo Alvarado Rodríguez',
     author_email='laalvarado@ine.gob.gt',
     description='Una colección de funciones para la manipulación de fechas.',
@@ -10,7 +10,7 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/1u1s4/funcionesjo',
     keywords='development, setup, setuptools',
-    python_requires='>=3.9',
+    python_requires='>=3',
     packages=find_packages(),
     py_modules=['funcionesjo'],
     install_requires=['python-dateutil']
