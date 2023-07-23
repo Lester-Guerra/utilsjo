e762110a80a72845b07b8506415543267f56650f
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-07-23 00:10:19 -0600

    utilsjo

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='funcionesjo',
-    version='0.2.7',
+    name='utilsjo',
+    version='1.0',
     author='Luis Alfredo Alvarado Rodríguez',
     author_email='laalvarado@ine.gob.gt',
-    description='Una colección de funciones para la manipulación de fechas.',
+    description='Una colección de funciones para la manipulación de fechas y muchas cosas mas.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/1u1s4/funcionesjo',
+    url='https://github.com/1u1s4/utilsjo',
     keywords='development, setup, setuptools',
-    python_requires='>=3',
+    python_requires='>=3.7',
     packages=find_packages(),
-    py_modules=['funcionesjo'],
+    py_modules=['utilsjo'],
     install_requires=['python-dateutil']
 )
---
+++ utilsjo/__init__.py
@@ -0,0 +1,1 @@
+from .utilsjo import *
